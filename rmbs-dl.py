3f5f6fa6393e9da589280099eb616fd8e482967f
azarrias <adolfo.zarrias@gmail.com>
2018-05-04 14:55:13 +0200

    Pagination for all results that meet the criteria
    TODO: Should be giving some feedback to the user, since some search
    criteria might require for hundreds of http requests

--- rmbs-dl.py
+++ rmbs-dl.py
@@ -1,6 +1,7 @@
 #!python2
 #coding: utf-8
 from bs4 import BeautifulSoup
+from collections import namedtuple
 import requests
 import sys
 
@@ -9,11 +10,33 @@ SEARCH_BUTTON_STR = u"&boton=Buscar"
 ALPHABET_LST = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', u'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 BOM_UTF8_LEN = 3
 
-def searchKeyword(str):
-   result = requests.post(BASE_URL_STR + str + SEARCH_BUTTON_STR)
-   decoded_result = result.content[BOM_UTF8_LEN:]
-   soup = BeautifulSoup(decoded_result, 'html.parser')
-   result_info = soup.find("p", { "class" : "resultado" })
+def performRequest(url):
+   result = requests.post(url)
+   trimmed_result = result.content[BOM_UTF8_LEN:]
+   return BeautifulSoup(trimmed_result, 'html.parser')
+
+def searchKeyword(criteria):
+   page_number = 1
+   book_list = []
+   while True:
+      soup = performRequest(BASE_URL_STR + criteria + SEARCH_BUTTON_STR + "&pg=" + str(page_number))
+      if page_number == 1:
+         result_info = soup.find("p", { "class" : "resultado" })
+         num_results = result_info.text.split()[4]
+         num_results = int(num_results)
+      bookcase = soup.find("ul", { "id" : "cajalibros" })
+      books = bookcase.findAll("li")
+      for book in books:
+         Book = namedtuple("Book", "title author url")
+         title = book.find("span", { "class" : "titulocondicionado" })
+         author = book.find("span", { "class" : "autor" })
+         url = book.find("a")["href"]
+         b = Book(title, author, url)
+         book_list.append(b)
+      if len(book_list) < num_results:
+         page_number += 1
+      else:
+	     break
    print result_info.text
 
 def promptForInput():
