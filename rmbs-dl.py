9e0600687c81be412f3928b87c1317267a455f5a
azarrias <adolfo.zarrias@gmail.com>
2018-05-07 18:31:21 +0200

    Pagination improved
    TODO: Merge MESSAGE_STR global list into OPTIONS dictionary, fix
    searchKeyword logic

--- rmbs-dl.py
+++ rmbs-dl.py
@@ -9,6 +9,18 @@ BASE_URL_STR = u"http://www.rmbs.es/catalogo.php?criterio="
 SEARCH_BUTTON_STR = u"&boton=Buscar"
 ALPHABET_LST = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', u'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 BOM_UTF8_LEN = 3
+OPTIONS = { "BROWSE_AUTHORS" : u"[A-Z] para buscar autores por orden alfabético",
+   "SEARCH_KEYWORDS" : u"Cualquier cadena para búsqueda por palabras clave",
+   "EXIT" : u"[0] para salir",
+   "DISPLAY_MORE" : u"[P] para visualizar otra página de títulos",
+   "DISPLAY_ALL" : u"[T] para visualizar todo (puede tardar según criterio)",
+   "DOWNLOAD" : u"Número a la izquierda del título para descargarlo",
+   "DOWNLOAD_ALL" : u"[D] para descargar todos los títulos devueltos" }
+STR_QUIT = u"Hasta luego!"
+MESSAGE_STR = [u"[A-Z] para buscar autores por orden alfabético", u"Cualquier cadena para búsqueda por palabras clave", 
+   u"[0] para salir", u"[P] para visualizar otra página de títulos", u"[T] para visualizar todo (puede tardar según criterio)",
+   u"Número a la izquierda del título para descargarlo", u"[D] para descargar todos los títulos devueltos", u"Hasta luego!", 
+   u"Buscar por autor", ""]
 
 def performRequest(url):
    result = requests.post(url)
@@ -18,12 +30,16 @@ def performRequest(url):
 def searchKeyword(criteria):
    page_number = 1
    book_list = []
+   paginate = True
+   soup = performRequest(BASE_URL_STR + criteria + SEARCH_BUTTON_STR + "&pg=" + str(page_number))
+   result_info = soup.find("p", { "class" : "resultado" })
+   num_results = result_info.text.split()[4]
+   print result_info.text + "\n"
+   if len(result_info.text.split()) <= 8:
+      return
+   num_results = int(num_results)   
    while True:
       soup = performRequest(BASE_URL_STR + criteria + SEARCH_BUTTON_STR + "&pg=" + str(page_number))
-      if page_number == 1:
-         result_info = soup.find("p", { "class" : "resultado" })
-         num_results = result_info.text.split()[4]
-         num_results = int(num_results)
       bookcase = soup.find("ul", { "id" : "cajalibros" })
       books = bookcase.findAll("li")
       for book in books:
@@ -33,24 +49,45 @@ def searchKeyword(criteria):
          url = book.find("a")["href"]
          b = Book(title, author, url)
          book_list.append(b)
+         print str(len(book_list)) + ") " + title.text + " [" + author.text + "]"
+      if paginate:
+         valid_option = False
+         for m in [MESSAGE_STR[i] for i in [9, 5, 6]]: print m
+         if len(book_list) < num_results:
+	        for m in [MESSAGE_STR[i] for i in [3, 4]]: print m
+         else:
+	        break
+         while not valid_option:
+            user_input = raw_input("> ")
+            if len(book_list) < num_results and user_input.upper() == "P":
+               valid_option = True
+            elif len(book_list) < num_results and user_input.upper() == "T":
+               paginate = False
+               valid_option = True
       if len(book_list) < num_results:
          page_number += 1
       else:
-	     break
-   print result_info.text
+         break
+#elif user_input.upper() == "D":
+#elif descarga titulo concreto
 
-def promptForInput():
-   print u"Buscar título por palabra clave:"
-   print u"[0] para salir"
-   print u"[A-Z] para buscar autores por orden alfabético"
+def userPrompt(option_list):
+   for m in [OPTIONS[key] for key in option_list]: print m
    user_input = raw_input("> ")
-   criteria = "+".join(user_input.split()).decode(sys.stdin.encoding)
-   if criteria == "0":
-      print "Hasta luego!"
-   elif criteria.upper() in ALPHABET_LST:
-      print "Buscar por autor"
+   user_command = "+".join(user_input.split()).decode(sys.stdin.encoding)
+   if user_command == "0" and "EXIT" in option_list:
+      print OPTIONS["EXIT"]
+      quit()
+   elif user_command.upper() in ALPHABET_LST and "BROWSE_AUTHORS" in option_list:
+      print u"Búsqueda por autor"
+      print
+   elif user_command.strip() != "" and "SEARCH_KEYWORDS" in option_list:
+      print
+      searchKeyword(user_command)
    else:
-      searchKeyword(criteria)
-      promptForInput()
+      print u"Opción no válida!"
+      print
+      userPrompt(option_list)
 
-promptForInput()
+while True:
+   command = userPrompt(["EXIT", "BROWSE_AUTHORS", "SEARCH_KEYWORDS"])
